2393961db7a108707f507cd69356c0de0a56acac
Albert-Valdemora <albertvaldemorat@gmail.com>
2024-10-31 21:46:16 -0400

    Arreglando API

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,9 @@ import { AuthContext } from "./auth/authContext";
 import { AppRouter } from "./components/routers/AppRouter";
 import { useHttp } from "./hooks/useHttp";
 
-// const urlSerie = 'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=es-ES&page=2&sort_by=popularity.desc&api_key=b0a9ee189c3906a9d1b7278103141c0d';
 const urlSerie = 'https://api.themoviedb.org/3/tv/popular?api_key=b0a9ee189c3906a9d1b7278103141c0d';
-// const urlSeriesMasVistas = 'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=es-ES&page=1&sort_by=popularity.desc&api_key=b0a9ee189c3906a9d1b7278103141c0d';
 const urlSeriesMasVistas = 'https://api.themoviedb.org/3/trending/tv/day?api_key=b0a9ee189c3906a9d1b7278103141c0d';
-// const urlTodasLasSeries = 'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=es-ES&page=2&sort_by=popularity.desc&api_key=b0a9ee189c3906a9d1b7278103141c0d';
 const urlTodasLasSeries = 'https://api.themoviedb.org/3/discover/tv?api_key=b0a9ee189c3906a9d1b7278103141c0d';
-// const urlTodasLasPeliculas = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=es-ES&page=1&sort_by=popularity.desc&api_key=b0a9ee189c3906a9d1b7278103141c0d'
 const urlTodasLasPeliculas = 'https://api.themoviedb.org/3/discover/movie?api_key=b0a9ee189c3906a9d1b7278103141c0d'
 const urlTopSerie = 'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=es-ES&page=1&sort_by=popularity.desc&api_key=b0a9ee189c3906a9d1b7278103141c0d'
 
